d01e18ca9da7c0bb91e02a20f7248b7d72eb6143
RichardRodi <RichardmRodi@gmail.com>
2024-02-29 15:20:50 -0500

    Added a TempConverter Class and Methods to convert Celsius to Fahrenheit and vice versa

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = TempConverter.FahrenheitToCelsius(44.3);
+            Console.WriteLine($"44.3 Degrees Fahrenheit is {answer} Celsius\n");
+
+
+            var answer2 = TempConverter.CelsiusToFarenheit(56.8);
+            Console.WriteLine($"56.8 degrees Celsius is {answer2} Fahrenheit"); 
+
         }
-    }
+
+    }           
 }
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double degree)
+
+        {
+            return (degree - 32) * 5 / 9;
+
+        }
+        public static double CelsiusToFarenheit(double degree)
+        {
+            return (degree * 9 / 5) + 32;
+        }
+    }
+}
